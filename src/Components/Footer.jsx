d0598beabf4eae25527b7371af0a55e1ec03ac3e
Alejo <alejojpa2@gmail.com>
2024-10-12 14:13:12 +0200

    Foot.jsx created and imported into EventPlanner.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'recat';
+const Footer = () =>{
+    retur (
+        <footer>
+            <p>&copy; ALEJO-Event-Planner Organization. All rights reserved. </p>
+        </footer>
+)}
+export default Footer
